f3e85625459ae10e5579221882044d1d7b282b58
huangrunhong <huang.run.hong.1986@gmail.com>
2023-10-26 18:13:43 +0200

    first change

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,22 +10,22 @@ const steuerOutput = document.body.querySelector("#steuerOutput");
 const bruttoOutput = document.body.querySelector("#bruttoOutput");
 
 const berechner = () => {
-  if (nettoZuBrutto.checked === true) {
-    if (highTax.checked === true) {
+  if (nettoZuBrutto.checked) {
+    if (highTax.checked) {
       steuerOutput.textContent = `${(nettoInput.value * 0.19).toFixed(2)} €`;
       bruttoOutput.textContent = `${(nettoInput.value * 1.19).toFixed(2)} €`;
-    } else if (lowTax.checked === true) {
+    } else if (lowTax.checked) {
       steuerOutput.textContent = `${(nettoInput.value * 0.07).toFixed(2)} €`;
       bruttoOutput.textContent = `${(nettoInput.value * 1.07).toFixed(2)} €`;
     }
-  } else if (bruttoZuNetto.checked === true) {
-    if (highTax.checked === true) {
+  } else if (bruttoZuNetto.checked) {
+    if (highTax.checked) {
       steuerOutput.textContent = `${(
         nettoInput.value -
         nettoInput.value / 1.19
       ).toFixed(2)} €`;
       bruttoOutput.textContent = `${(nettoInput.value / 1.19).toFixed(2)} €`;
-    } else if (lowTax.checked === true) {
+    } else if (lowTax.checked) {
       steuerOutput.textContent = `${(
         nettoInput.value -
         nettoInput.value / 1.07
